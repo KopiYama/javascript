8cfa67dc393f9feed3c20e9f0d069bd586bc8111
KopiYama <mrrizkya46@gmail.com>
2023-11-18 13:09:09 +0700

    percobaan2 if dan else

--- CobaCode.js
+++ CobaCode.js
@@ -1,7 +1,17 @@
 const isLate = true;
+const cuaca = 'hujan';
+
 
 if (isLate) {
     console.log('masuk');
 } else {
     console.log('sarapan');
 }
+
+if (cuaca === 'hujan') {
+    console.log('Bawa payung');
+} else if (cuaca === 'dingin') {
+    console.log('Bawa jaket');
+}
+
+console.log("lesgoo berangkat");
